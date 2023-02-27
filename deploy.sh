b4f90b71e54976cb4efd1760055e584231600b83
Zack Brown <zack@inclination.co>
2023-02-27 12:48:22 -0800

    update CDN distro ID in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 aws --profile=inclination s3 cp --recursive --acl=public-read ./src/ s3://www.pax.rs/
-aws --profile=inclination cloudfront create-invalidation --distribution-id=E2VGVLF1W3Z66M --paths "/*"
+aws --profile=inclination cloudfront create-invalidation --distribution-id=E2IPWLWQB0SRVA --paths "/*"
