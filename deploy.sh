8b41ac652e224e5ac0cdb0da7c3dcf11dc5b54a0
Zack Brown <zack@pax.rs>
2023-05-19 16:55:49 -0700

    update deployment for www.pax.dev

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws --profile=inclination s3 cp --recursive --acl=public-read ./src/ s3://www.pax.rs/
-aws --profile=inclination cloudfront create-invalidation --distribution-id=E2IPWLWQB0SRVA --paths "/*"
+aws --profile=pax s3 cp --recursive --acl=public-read ./src/ s3://www.pax.dev/
+aws --profile=pax cloudfront create-invalidation --distribution-id=E4XEPE4OLK651 --paths "/*"
