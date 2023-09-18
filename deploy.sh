dff60015f22aa975e5002213427f559d9b313579
Zack Brown <zack@pax.dev>
2023-09-18 16:59:33 -0700

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws --profile=pax s3 cp --recursive --acl=public-read ./src/ s3://www.pax.dev/
+aws --profile=pax s3 cp --recursive --acl=public-read . s3://www.pax.dev/
 aws --profile=pax cloudfront create-invalidation --distribution-id=E4XEPE4OLK651 --paths "/*"
