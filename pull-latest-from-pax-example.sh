34a76782227945152c45c93bd18f95457e7317a7
Zack Brown <zack@pax.dev>
2023-09-20 13:10:25 -0700

    adjust script to assume running from within monorepo

--- pull-latest-from-pax-example.sh
+++ pull-latest-from-pax-example.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cp -r ../pax/pax-example/.pax/build/Web/dist/* ./pax_modules/PaxWebsite/
+# Assumes we are calling this from within the monorepo
+cp -r ../pax-example/.pax/build/Web/dist/* ./pax_modules/PaxWebsite/
